84bcd08ec0e70b7f86178481926e60243a2fd798
Francis Kornu <franciskornu@gmail.com>
2018-08-29 15:09:27 +0000

    first commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+exports.OAUTHCREDENTIALS = {
+    googleClientID: '443386877511-qshghe18v4hcl9r3k58mar1gi3f1b2qt.apps.googleusercontent.com',
+    googleClientSecret: 'x9Bn1BfltqnlnQQv-8vyd7lP'
+};
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
-// passport.use(new GoogleStrategy({
-//         clientID: process.env.GOOGLE_CLIENT_ID,
-//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         callbackURL: "http://www.example.com/auth/google/callback"
-//     },
-//     function (accessToken, refreshToken, profile, cb) {
-//         User.findOrCreate({googleId: profile.id}, function (err, user) {
-//             return cb(err, user);
-//         });
-//
-//     }
-// ));
+passport.use(new GoogleStrategy({
+        clientID: process.env.GOOGLE_CLIENT_ID || keys.OAUTHCREDENTIALS.googleClientID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET || keys.OAUTHCREDENTIALS.googleClientSecret,
+        callbackURL: "/auth/google/callback"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        // User.findOrCreate({googleId: profile.id}, function (err, user) {
+        //     return cb(err, user);
+        // });
+
+        console.log("ACCESS TOKEN" + accessToken);
+    }
+));
 
 
 app.get('/', function (req, res) {
@@ -23,10 +25,9 @@ app.get('/', function (req, res) {
 
 });
 
-app.get('/auth/google', function (req, res) {
-
-
-});
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+}));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
