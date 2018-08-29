77d247744bc8a13687a0ea0ceb533f9b715869e3
Francis Kornu <franciskornu@gmail.com>
2018-08-29 12:16:27 +0000

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const app = express();
-const PORT = process.env.PORT || 5000;
+// passport.use(new GoogleStrategy({
+//         clientID: process.env.GOOGLE_CLIENT_ID,
+//         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//         callbackURL: "http://www.example.com/auth/google/callback"
+//     },
+//     function (accessToken, refreshToken, profile, cb) {
+//         User.findOrCreate({googleId: profile.id}, function (err, user) {
+//             return cb(err, user);
+//         });
+//
+//     }
+// ));
+
 
 app.get('/', function (req, res) {
 
@@ -8,5 +23,10 @@ app.get('/', function (req, res) {
 
 });
 
+app.get('/auth/google', function (req, res) {
 
+
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
